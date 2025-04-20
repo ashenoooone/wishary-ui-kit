2b15c477564051ef46075fc1591729d49b9d47fe
Roman Gontar <ashenoooone@gmail.com>
2025-04-20 15:29:55 +0400

    tailwind css colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <button className="text-brand-primary-400">Click me</button>
+      <button className="bg-brand-primary-1000">test</button>
+    </>
+  );
 }
 
 export default App;

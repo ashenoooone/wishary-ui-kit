ecf1e1008ad716a426b12cf92282a33fe1eb5529
Roman Gontar <ashenoooone@gmail.com>
2025-04-20 22:44:24 +0400

    typography component ready

---
+++ src/components/typography/index.tsx
@@ -0,0 +1,108 @@
+import { cn } from '@/lib/cn';
+
+type AsElement = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
+export type Variant =
+  // Large Title
+  | 'large-title-l'
+  | 'large-title-m'
+  | 'large-title-s'
+
+  // Title 1
+  | 'title-l-1'
+  | 'title-m-1'
+  | 'title-s-1'
+
+  // Title 2
+  | 'title-l-2'
+  | 'title-m-2'
+  | 'title-s-2'
+
+  // Title 3
+  | 'title-l-3'
+  | 'title-m-3'
+  | 'title-s-3'
+
+  // Headline
+  | 'headline-l'
+  | 'headline-m'
+  | 'headline-s'
+
+  // Callout
+  | 'callout-l'
+  | 'callout-m'
+  | 'callout-s'
+
+  // Body
+  | 'body-l'
+  | 'body-m'
+  | 'body-s'
+
+  // Subheadline
+  | 'subheadline-l'
+  | 'subheadline-m'
+  | 'subheadline-s'
+
+  // Footnote
+  | 'footnote-l'
+  | 'footnote-m'
+  | 'footnote-s'
+
+  // Caption 1
+  | 'caption-l-1'
+  | 'caption-m-1'
+  | 'caption-s-1'
+
+  // Caption 2
+  | 'caption-l-2'
+  | 'caption-m-2'
+  | 'caption-s-2';
+
+export type Props = {
+  as?: AsElement;
+  children: React.ReactNode;
+  variant?: Variant;
+};
+
+export const Typography = (props: Props) => {
+  const { as, variant = 'body-s', ...rest } = props;
+
+  const Component = as || 'p';
+
+  const variantStyles = {
+    'body-l': 'text-body-l',
+    'body-m': 'text-body-m',
+    'body-s': 'text-body-s',
+    'subheadline-l': 'text-subheadline-l',
+    'subheadline-m': 'text-subheadline-m',
+    'subheadline-s': 'text-subheadline-s',
+    'footnote-l': 'text-footnote-l',
+    'footnote-m': 'text-footnote-m',
+    'footnote-s': 'text-footnote-s',
+    'caption-l-1': 'text-caption-l-1',
+    'caption-m-1': 'text-caption-m-1',
+    'caption-s-1': 'text-caption-s-1',
+    'caption-l-2': 'text-caption-l-2',
+    'caption-m-2': 'text-caption-m-2',
+    'caption-s-2': 'text-caption-s-2',
+    'large-title-l': 'text-large-title-l',
+    'large-title-m': 'text-large-title-m',
+    'large-title-s': 'text-large-title-s',
+    'title-l-1': 'text-title-l-1',
+    'title-m-1': 'text-title-m-1',
+    'title-s-1': 'text-title-s-1',
+    'title-l-2': 'text-title-l-2',
+    'title-m-2': 'text-title-m-2',
+    'title-s-2': 'text-title-s-2',
+    'title-l-3': 'text-title-l-3',
+    'title-m-3': 'text-title-m-3',
+    'title-s-3': 'text-title-s-3',
+    'callout-l': 'text-callout-l',
+    'callout-m': 'text-callout-m',
+    'callout-s': 'text-callout-s',
+    'headline-l': 'text-headline-l',
+    'headline-m': 'text-headline-m',
+    'headline-s': 'text-headline-s',
+  }[variant];
+
+  return <Component {...rest} className={cn(variantStyles)} />;
+};
---
+++ src/components/typography/typography.stories.tsx
@@ -0,0 +1,248 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Typography } from './index';
+
+const meta = {
+  component: Typography,
+  title: 'Typography',
+  tags: ['autodocs'],
+} satisfies Meta<typeof Typography>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Hello World',
+  },
+};
+
+export const LargeTitleL: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'large-title-l',
+  },
+};
+
+export const LargeTitleM: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'large-title-m',
+  },
+};
+
+export const LargeTitleS: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'large-title-s',
+  },
+};
+
+export const TitleL1: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-l-1',
+  },
+};
+
+export const TitleM1: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-m-1',
+  },
+};
+
+export const TitleS1: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-s-1',
+  },
+};
+
+export const TitleL2: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-l-2',
+  },
+};
+
+export const TitleM2: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-m-2',
+  },
+};
+
+export const TitleS2: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-s-2',
+  },
+};
+
+export const TitleL3: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-l-3',
+  },
+};
+
+export const TitleM3: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-m-3',
+  },
+};
+
+export const TitleS3: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'title-s-3',
+  },
+};
+
+export const CalloutL: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'callout-l',
+  },
+};
+
+export const CalloutM: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'callout-m',
+  },
+};
+
+export const CalloutS: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'callout-s',
+  },
+};
+
+export const HeadlineL: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'headline-l',
+  },
+};
+
+export const HeadlineM: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'headline-m',
+  },
+};
+
+export const HeadlineS: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'headline-s',
+  },
+};
+
+export const BodyL: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'body-l',
+  },
+};
+
+export const BodyM: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'body-m',
+  },
+};
+
+export const BodyS: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'body-s',
+  },
+};
+
+export const SubheadlineL: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'subheadline-l',
+  },
+};
+
+export const SubheadlineM: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'subheadline-m',
+  },
+};
+
+export const SubheadlineS: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'subheadline-s',
+  },
+};
+
+export const FootnoteL: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'footnote-l',
+  },
+};
+
+export const FootnoteM: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'footnote-m',
+  },
+};
+
+export const FootnoteS: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'footnote-s',
+  },
+};
+
+export const CaptionL1: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'caption-l-1',
+  },
+};
+
+export const CaptionM1: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'caption-m-1',
+  },
+};
+
+export const CaptionS1: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'caption-s-1',
+  },
+};
+
+export const CaptionL2: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'caption-l-2',
+  },
+};
+
+export const CaptionM2: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'caption-m-2',
+  },
+};
+
+export const CaptionS2: Story = {
+  args: {
+    children: 'Hello World',
+    variant: 'caption-s-2',
+  },
+};

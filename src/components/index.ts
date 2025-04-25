751c240f394ef5c2f22bbc256cfc1e94880dfe53
Roman Gontar <ashenoooone@gmail.com>
2025-04-25 23:04:39 +0400

    wip: project build

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { Button } from './button';
+import { Typography } from './typography';
+import { HSpacing } from './h-spacing';
+import { VSpacing } from './v-spacing';
+
+export { Button, Typography, HSpacing, VSpacing };
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,68 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path, { extname, relative } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
-// https://vite.dev/config/
+const entries = Object.fromEntries(
+  glob
+    .sync('src/components/**/*.{ts,tsx}', {
+      ignore: ['**/*.stories.tsx', '**/*.stories.ts'],
+    })
+    .map(file => [
+      relative('src/components', file.slice(0, file.length - extname(file).length)),
+      fileURLToPath(new URL(file, import.meta.url)),
+    ]),
+);
+
+const outputBase = {
+  globals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    clsx: 'clsx',
+    'tailwind-merge': 'tailwind-merge',
+  },
+};
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: './dist',
+    ssr: true,
+    copyPublicDir: false,
+    // https://vitejs.dev/config/build-options.html#build-rollupoptions
+    rollupOptions: {
+      external: ['react', 'react-dom', 'clsx', 'tailwind-merge'],
+      input: entries,
+      output: [
+        {
+          ...outputBase,
+          exports: 'named',
+          format: 'cjs',
+          esModule: true,
+        },
+        {
+          ...outputBase,
+          exports: 'named',
+          format: 'esm',
+          interop: 'esModule',
+        },
+      ],
+      plugins: [],
     },
   },
 });

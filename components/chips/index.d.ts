3a090916a8d0143afa6343691a9c66cae5d54d25
ashenoooone <ashenoooone@users.noreply.github.com>
2025-04-26 17:34:28 +0000

    Deploy Storybook v0.0.1 to GitHub Pages

---
+++ components/chips/index.d.ts
@@ -0,0 +1,6 @@
+import { BaseStylesProps } from '../../lib/types';
+type Props = {
+    asChild?: boolean;
+} & BaseStylesProps & React.ComponentProps<'button'>;
+export declare const Chips: (props: Props) => import("react/jsx-runtime").JSX.Element;
+export {};
